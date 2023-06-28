4fbf5dfc30cdc5d57698565d3711d0aeee9a80ff
hyottly <hyottly@gmail.com>
2023-06-28 23:11:37 +0900

    230628 p311 상속,(캐스팅연산자- 자식타입) 부모타입 객체 강제타입 변환

---
+++ 김효슬/webmon_khs/src/webmon_khs_w01/Child.java
@@ -0,0 +1,13 @@
+package webmon_khs_w01;
+//p311 상속, 부모타입 강제 타입 변환
+public class Child extends Parent {//자식클래스
+	//필드 선언
+	public String field2;
+	
+	//메소드 선언
+	public void method3() {
+		System.out.println("Child-method3()");
+	}
+		
+	}
+
