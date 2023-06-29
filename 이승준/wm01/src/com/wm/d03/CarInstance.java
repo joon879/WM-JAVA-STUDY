2d9d0abe499991f384a7fa2244d40670f3905418
joon879 <joon879@gmail.com>
2023-06-30 02:29:09 +0900

    Day03: 스태틱에서 인스턴스 멤버로의 접근 불가

---
+++ 이승준/wm01/src/com/wm/d03/CarInstance.java
@@ -0,0 +1,23 @@
+package com.wm.d03;
+
+public class CarInstance {
+	int speed;
+	
+	void run() {
+		System.out.println(speed + "Ŕ¸ˇÎ ´Ţ¸ł´Ď´Ů.");
+	}
+	
+	static void simulate() {
+		CarInstance myCar = new CarInstance();
+		myCar.speed = 200;
+		myCar.run();
+	}
+	
+	public static void main(String[] args) {
+		simulate();
+		
+		CarInstance myCar = new CarInstance();
+		myCar.speed = 60;
+		myCar.run();
+	}
+}
