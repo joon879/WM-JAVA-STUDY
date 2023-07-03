46781421f9d4a05b494280e67c13122c31e9b994
jsk970427 <jsk970427@hanmail.net>
2023-07-03 20:27:21 +0900

    진성 DAY06 Chapter11. 예외 처리

---
+++ 김진성/javastudy/src/com/wm/d06/InsufficientException.java
@@ -0,0 +1,14 @@
+package com.wm.d06;
+
+public class InsufficientException extends Exception {
+	
+	public InsufficientException() {
+	}
+	
+
+	public InsufficientException(String message) {
+		super(message);
+	}
+	
+	
+}
