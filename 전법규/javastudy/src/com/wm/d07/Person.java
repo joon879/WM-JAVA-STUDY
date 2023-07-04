d7f65f5fd1c10a2e6d63604ac8f4823aeb16c218
wkqk <wkqkqmffhrm00@naver.com>
2023-07-04 21:33:04 +0900

    d.07

---
+++ 전법규/javastudy/src/com/wm/d07/Person.java
@@ -0,0 +1,24 @@
+package com.wm.d07;
+
+public class Person {
+
+}
+
+class Worker extends Person{	// Person 클래스 상속
+	
+}
+
+class Student extends Person{	// Person 클래스 상속
+	
+}
+class MiddleStudent extends Student{	// Person 클래스를 상속받는 Student 상속
+	
+}
+class HighStudent extends Student{		// Person 클래스를 상속받는 Student 상속
+	
+}
+
+
+
+
+
