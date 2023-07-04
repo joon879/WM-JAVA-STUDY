0d567d2c6b7a569c808268092600f507918883a2
hyottly <hyottly@gmail.com>
2023-07-04 22:04:56 +0900

    // 230705 예외처리(throw, try-catch문) 수정본(주석 추가) (throw+Exception, RuntimeException, InsufficientException)

--- 김효슬/webmon_khs/src/webmon_khs_w02/AccountExample.java
+++ 김효슬/webmon_khs/src/webmon_khs_w02/AccountExample.java
@@ -23,5 +23,11 @@ public class AccountExample {
 		System.out.println(message);
 		}
 	}
-
 }
+//위 코드는 AccountExample 클래스로, Account 클래스를 사용하는 예시 코드입니다.
+//먼저 Account 객체 account를 생성합니다.그리고 account 객체를 사용하여 예금을 진행합니다.
+//account.deposit(10000); 코드를 통해 10000원을 예금합니다.
+//그 후 account.getBalance()를 호출하여 현재 잔액을 출력합니다.
+//이어서 출금을 진행합니다. account.withdraw(30000); 코드를 호출합니다. 이때 출금액이 잔액보다 크기 때문에 InsufficientResourcesException 예외가 발생합니다.
+//catch 블록에서는 InsufficientResourcesException 예외를 처리합니다. 예외 객체의 메시지를 가져와 message 변수에 저장하고, 이를 출력합니다. 이를 통해 "잔고 부족: (부족한 금액) 모자람" 형태의 메시지가 출력됩니다.
+//이렇게 예외 처리를 통해 출금 시 잔고 부족 예외를 처리하는 예시 코드입니다.
