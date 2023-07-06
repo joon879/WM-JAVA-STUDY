7fb2b03336f2e41cc72fb0ed891a6ba7d522f443
jsk970427 <jsk970427@hanmail.net>
2023-07-06 21:36:54 +0900

    진성 DAY08 Chapter14. 멀티 스레드

---
+++ 김진성/javastudy/src/com/wm/d08/Calculator.java
@@ -0,0 +1,27 @@
+package com.wm.d08;
+
+public class Calculator {
+	private int memory;
+
+	public int getMoney() {
+		return memory;
+	}
+
+	public synchronized void setMemory1(int memory) {
+		this.memory = memory;
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {}
+		System.out.println(Thread.currentThread().getName() + ":" + this.memory);
+	}
+
+	public void setMemory2(int memory) {
+		synchronized (this) {
+			this.memory = memory;
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {}
+			System.out.println(Thread.currentThread().getName() + ":" + this.memory);
+		}
+	}
+}
---
+++ 김진성/javastudy/src/com/wm/d08/JoinExample.java
@@ -0,0 +1,13 @@
+package com.wm.d08;
+
+public class JoinExample {
+	public static void main(String[] args) {
+		SumThread sumThread = new SumThread();
+		sumThread.start();
+		try {
+			sumThread.join();
+		} catch (InterruptedException e) {
+		}
+		System.out.println("1~100 гу : "+sumThread.getSum());	
+	}
+}
